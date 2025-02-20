7a867df91c83369f71e1868ce215764467744d20
Tina Wulff <tina@tinawulff.dk>
2025-02-20 12:18:47 +0100

    header creat start

--- home.pokelist.js
+++ home.pokelist.js
@@ -10,3 +10,30 @@ fetch("https://pokeapi.co/api/v2/pokemon", {
     .then((data) => {
         console.log(data)
     })
+
+    .catch((error) => {
+        console.error("Der opstod en fejl:", error);
+    });
+
+let docBody = document.querySelector("body");
+
+let header = document.createElement("header");      //header created in body
+
+let logoImg = document.createElement("img");        //LOGO IMG created              //To be APPENDED IN HEADER
+logoImg.classList.add("logoImg");
+logoImg.src = "img/logo.pokeball.svg"
+logoImg.alt = "logo";
+
+let logoTitle = document.createElement("h1");    //headline created
+logoTitle.innerHTML = "Pokédex";
+
+
+//APPENDS
+header.append(logoImg, logoTitle);
+docBody.append(header);
+
+
+
+
+
+
